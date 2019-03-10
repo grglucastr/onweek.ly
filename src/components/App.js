09e22bb08ce6f6d84d14e6c4619b348f08656200
George Lucas T. Bentes <george.bentes@gmail.com>
2019-03-10 18:46:06 -0400

    Change file reference names

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
 
-import Header from './Header'
+import AppHeader from './AppHeader'
+import AppFooter from './AppFooter'
+
 import Home from './Home'
+
 import TaskNew from './TaskNew'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header />
+        <AppHeader />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/tasks" component={TaskNew} />
         </Switch>
+        <AppFooter />
       </div>
     );
   }
