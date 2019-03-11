78c6cab47d4b7c40c6ac981ac3104d933acea56f
George Bentes <george.bentes@gmail.com>
2019-03-11 13:37:33 -0400

    Change in Footer Component and Home Component

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -1,77 +1,64 @@
-import React, { Fragment } from 'react'
-import {AppBar, withStyles, Typography, Link} from '@material-ui/core'
-
+import React, { Fragment } from "react";
+import { AppBar, withStyles, Typography, Link } from "@material-ui/core";
 
 const styles = theme => ({
-  appBar:{
-    top: 'auto',
-    bottom:0,
-    height:35,
-    backgroundColor:'#000',
-    display:'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center'
+  appBar: {
+    top: "auto",
+    bottom: 0,
+    height: 35,
+    backgroundColor: "#000",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
   },
-  
-  typo:{
-    color:'#fff',
-    fontSize:12,
-    width:'25%',
-    textAlign: 'center',
+
+  typo: {
+    border: "1px solid red",
+    color: "#fff",
+    fontSize: 12,
+    textAlign: "center"
   },
 
   contactSupport: {
-    cursor: 'pointer',
-    color:'#fff',
-    textTransform:'uppercase',
+    cursor: "pointer",
+    color: "#fff",
+    textTransform: "uppercase",
     letterSpacing: 2,
-    display: 'inline-block',
-    '&:hover':{
-      textDecoration: 'none'
+    display: "inline-block",
+    "&:hover": {
+      textDecoration: "none"
     }
   },
 
-  
-
   grow: {
     grow: 1,
-    width:'25%',
-  },
-})
+    width: "25%"
+  }
+});
 
-class AppFooter extends React.Component{
-  render(){
+class AppFooter extends React.Component {
+  render() {
+    const { classes } = this.props;
 
-    const {classes} = this.props
-
-    return(
+    return (
       <Fragment>
         <AppBar className={classes.appBar}>
-          <div className={classes.grow}></div>
-          <div className={classes.grow}></div>
-          <Typography className={classes.typo}>
+          <div style={{ display: "block" }}>
+            <Typography className={classes.typo}>
               OnWeek.ly - Copyright 2019
-          </Typography>
+            </Typography>
+          </div>
 
-          <div className={classes.grow}></div>
-
-          <Typography className={[classes.typo]}>
-            
-            <Link className={classes.contactSupport} >  
-              Contact Support
-            </Link>
-          </Typography>
-
-
-          
-
+          <div>
+            <Typography className={[classes.typo]}>
+              <Link className={classes.contactSupport}>Contact Support</Link>
+            </Typography>
+          </div>
         </AppBar>
       </Fragment>
-    )
+    );
   }
-
 }
 
-
-export default withStyles(styles)(AppFooter)
+export default withStyles(styles)(AppFooter);
