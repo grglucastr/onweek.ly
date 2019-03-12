53b3ca43a677f919bc66da23e2fbb26ffdc120ca
George Bentes <george.bentes@gmail.com>
2019-03-12 12:38:16 -0400

    Add redux store and provider to the app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import taskReducer from "./reducers/tasks";
+
+const store = createStore(taskReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
