64f5c485dc211c9de8931fb8ca252771a9c6f7db
George Bentes <george.bentes@gmail.com>
2019-03-12 13:12:09 -0400

    List tasks in TaskHome component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,56 @@
 import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import AppHeader from "./AppHeader";
 import AppFooter from "./AppFooter";
-
 import TaskHome from "./TaskHome";
 import TaskForm from "./TaskForm";
 
+import { listTasks } from "../actions/tasks";
+
+const tasks = [
+  {
+    id: 1,
+    subject: "Fix Access Point",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    start_date: "2019-03-12",
+    expected_end_date: "2019-03-15",
+    status: 1
+  },
+  {
+    id: 2,
+    subject: "Ip Connection Problem",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    start_date: "2019-03-11",
+    expected_end_date: "2019-03-13",
+    status: 2
+  },
+
+  {
+    id: 3,
+    subject: "Add Corp proxy for new PCs",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    start_date: "2019-03-12",
+    expected_end_date: "2019-03-12",
+    status: 2
+  },
+
+  {
+    id: 4,
+    subject: "Fix proxy breaches",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    start_date: "2019-03-12",
+    expected_end_date: "2019-03-14",
+    status: 3
+  }
+];
+
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(listTasks(tasks));
+  }
+
   render() {
     return (
       <Router>
@@ -25,4 +68,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect()(App);
--- src/components/TaskHome.js
+++ src/components/TaskHome.js
@@ -1,19 +1,49 @@
 import React, { Component } from "react";
-
+import { connect } from "react-redux";
 
 class Home extends Component {
   render() {
-  
+    const { tasks } = this.props;
+    let planTasks = [];
+    let progressTasks = [];
+    let doneTasks = [];
+
+    if (tasks) {
+      planTasks = tasks.filter(x => x.status === 1);
+      doneTasks = tasks.filter(x => x.status === 3);
+      progressTasks = tasks.filter(x => x.status === 2);
+    }
+
     return (
       <div>
         <h3>Tasks Listing Home</h3>
-        
+        <p>Plan</p>
+        <ul>
+          {planTasks.map(task => (
+            <li key={task.id}>{task.subject}</li>
+          ))}
+        </ul>
+
+        <p>In Progress</p>
+        <ul>
+          {progressTasks.map(task => (
+            <li key={task.id}>{task.subject}</li>
+          ))}
+        </ul>
+
+        <p>Done</p>
+        <ul>
+          {doneTasks.map(task => (
+            <li key={task.id}>{task.subject}</li>
+          ))}
+        </ul>
       </div>
-      
     );
   }
 }
 
+function mapStateToProps(tasks) {
+  return { tasks };
+}
 
-
-export default Home;
+export default connect(mapStateToProps)(Home);
