0379a7ad0223653c8c4b4784e1f762b297360aa9
George Lucas T. Bentes <george.bentes@gmail.com>
2019-03-16 20:50:24 -0400

    Add modal for item clicked

--- src/components/TaskCardsItem.js
+++ src/components/TaskCardsItem.js
@@ -1,14 +1,53 @@
 import React from "react"
-import ListGroup from "react-bootstrap/ListGroup";
 
-const TaskCardsItem = ({task}) => {
-  return(
-   <>
-    <ListGroup.Item action href={`#task_${task.id}`} onClick={() => console.log('I clicked here')}>
-      {task.subject}
-    </ListGroup.Item>
-   </>
-  )
+import ListGroup from "react-bootstrap/ListGroup";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+import TaskDetailInfo from './TaskDetailInfo';
+
+
+
+class TaskCardsItem extends React.Component {
+  state = {
+    showModal: false
+  }
+
+
+
+  render() {
+    const { task }  = this.props;
+    return (
+      <>
+        <ListGroup.Item
+          action
+          onClick={() => this.setState({showModal: true})}>
+          {task.subject}
+        </ListGroup.Item>
+
+        <Modal
+          show={this.state.showModal}
+          onHide={() => this.setState({showModal: false})}>
+          <Modal.Header>
+            <strong>Tasks Details</strong>
+          </Modal.Header>
+
+          <Modal.Body>
+            <TaskDetailInfo task={task}  />
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button 
+              variant="secondary" 
+              onClick={() => this.setState({showModal: false})}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
+      </>
+    )
+  }
 }
 
 export default TaskCardsItem;
